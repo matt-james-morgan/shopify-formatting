012694592dbba5aea51e4dcd5cc7257d43c69faf
Peginaw <simon.pequegnat@gmail.com>
2024-03-21 12:21:39 -0400

    add products_export.csv

--- script.py
+++ script.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('products_export.csv')
+
+df['Title'] = df['Title'].replace("[a-z A-Z]+ ?- ?", "", regex=True).str.upper()
+
+print(df["Title"])
